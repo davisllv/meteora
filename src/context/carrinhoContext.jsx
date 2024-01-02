58b0a27cc14f8842a2d0b7d5ab7bf4e02c2f4c22
davisllv <davisllvsnt57@gmail.com>
2024-01-01 21:11:17 -0300

    created and inserted the values in context

--- src/context/carrinhoContext.jsx
+++ src/context/carrinhoContext.jsx
@@ -1,8 +1,76 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const CarrinhoContext = createContext();
 
 // Define um provider, caso contrário seria necessário criar dessa forma no componente que o utiliza.
 export const CarrinhoProvider = ({ children }) => {
-  return <CarrinhoContext.Provider> {children} </CarrinhoContext.Provider>;
+  const [carrinho, setCarrinho] = useState([]);
+  const [quantidadeProdutos, setQuantidadeProdutos] = useState(0);
+  const [valorTotalCarrinho, setValorTotalCarrinho] = useState(0);
+
+  function mudarQuantidade(id, quantidade) {
+    return carrinho.map((itemDoCarrinho) => {
+      if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade += quantidade;
+      return itemDoCarrinho;
+    });
+  }
+
+  function adicionarProduto(novoProduto) {
+    const temOProduto = carrinho.some(
+      (itemDoCarrinho) => itemDoCarrinho.id === novoProduto.id
+    );
+    if (!temOProduto) {
+      novoProduto.quantidade = 1;
+      return setCarrinho((carrinhoAnterior) => [
+        ...carrinhoAnterior,
+        novoProduto,
+      ]);
+    }
+    setCarrinho(mudarQuantidade(novoProduto.id, 1));
+  }
+
+  function removerProduto(id) {
+    const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
+    const ehOUltimo = produto.quantidade === 1;
+    if (ehOUltimo) {
+      return setCarrinho((carrinhoAnterior) =>
+        carrinhoAnterior.filter((itemDoCarrinho) => itemDoCarrinho.id !== id)
+      );
+    }
+    setCarrinho(mudarQuantidade(id, -1));
+  }
+
+  function removerProdutoCarrinho(id) {
+    const produto = carrinho.filter(
+      (itemDoCarrinho) => itemDoCarrinho.id !== id
+    );
+    setCarrinho(produto);
+  }
+
+  useEffect(() => {
+    const { novoTotal, novaQuantidade } = carrinho.reduce(
+      (contador, produto) => ({
+        novaQuantidade: contador.novaQuantidade + produto.quantidade,
+        novoTotal: contador.novoTotal + produto.preco * produto.quantidade,
+      }),
+      {
+        novaQuantidade: 0,
+        novoTotal: 0,
+      }
+    );
+    setQuantidadeProdutos(novaQuantidade);
+    setValorTotalCarrinho(novoTotal);
+  }, [carrinho, setQuantidadeProdutos, setValorTotalCarrinho]);
+
+  return (
+    <CarrinhoContext.Provider
+      value={{
+        carrinho,
+        setCarrinho,
+      }}
+    >
+      {" "}
+      {children}{" "}
+    </CarrinhoContext.Provider>
+  );
 };
